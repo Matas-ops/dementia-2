dea1e6658583e185036f351b26551c6a570330fd
Matas-ops <matukas.jurevicius@gmail.com>
2025-12-15 19:17:39 +0200

    doctype

--- src/server.py
+++ src/server.py
@@ -9,14 +9,14 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>idk</title></head>", "utf-8"))
+        self.wfile.write(bytes("<!DOCTYPE html><head><title>idk</title></head>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>Yuh</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 
 def run_dummy_server():
-    # idiotic way to make sure google cloud is satisfied
+    # a way to make sure google cloud is satisfied
     print("Starting server...")
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
