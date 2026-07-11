3cf4937b480803fa76813caed2ecdcb75938704d
Matas <matukas.jurevicius@gmail.com>
2026-07-12 00:00:41 +0300

    add back tick tack toe

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 #import threading
 #from server import run_dummy_server
-#from tictactoe import tic_tac_toe, init_tictactoe
+from tictactoe import tic_tac_toe, init_tictactoe
 from gif import gif, init_gifs, fetch_gifs
 from quote import quote, init_quote, fetch_quote_of_day, fetch_verse_of_day
 from remind import remind, init_remind
@@ -15,7 +15,7 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-#bot.add_command(tic_tac_toe)
+bot.add_command(tic_tac_toe)
 bot.add_command(gif)
 bot.add_command(quote)
 bot.add_command(remind)
@@ -23,7 +23,7 @@ bot.add_command(tiktok_all)
 bot.add_command(tiktok_55)
 bot.add_command(tiktok_gedipliusas)
 
-#init_tictactoe(bot)
+init_tictactoe(bot)
 #init_quote(os.getenv("QUOTE_API_KEY"), os.getenv("BIBLE_API_KEY"))
 init_gifs(bot, int(os.getenv("GIF_CHANNEL_ID")))
 init_remind(bot)
