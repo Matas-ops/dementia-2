324be0a100c941b4c3fc49b5b3803b1e1e5f3b1b
Matas <matukas.jurevicius@gmail.com>
2026-07-15 01:49:08 +0300

    disable tiktok

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from gif import gif, init_gifs, fetch_gifs
 from quote import quote, init_quote, fetch_quote_of_day, fetch_verse_of_day
 from remind import remind, init_remind
 from message_handler import handle_message
-from tiktok import tiktok_all, tiktok_55, tiktok_gedipliusas, init_tiktok_tokens, get_tiktok_videos
+#from tiktok import tiktok_all, tiktok_55, tiktok_gedipliusas, init_tiktok_tokens, get_tiktok_videos
 
 load_dotenv()
 
@@ -19,21 +19,21 @@ bot.add_command(tic_tac_toe)
 bot.add_command(gif)
 bot.add_command(quote)
 bot.add_command(remind)
-bot.add_command(tiktok_all)
-bot.add_command(tiktok_55)
-bot.add_command(tiktok_gedipliusas)
+#bot.add_command(tiktok_all)
+#bot.add_command(tiktok_55)
+#bot.add_command(tiktok_gedipliusas)
 
 init_tictactoe(bot)
 #init_quote(os.getenv("QUOTE_API_KEY"), os.getenv("BIBLE_API_KEY"))
 init_gifs(bot, int(os.getenv("GIF_CHANNEL_ID")))
 init_remind(bot)
-init_tiktok_tokens(os.getenv("TIKTOK_TOKENS").split(","))
+#init_tiktok_tokens(os.getenv("TIKTOK_TOKENS").split(","))
 
 @bot.event
 async def on_ready():
     print("Bot is ready.")
     fetch_gifs.start()
-    get_tiktok_videos.start()
+    #get_tiktok_videos.start()
     #fetch_quote_of_day.start()
     #fetch_verse_of_day.start()
 
